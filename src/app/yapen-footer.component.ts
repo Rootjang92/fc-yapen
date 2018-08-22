3dc0ae501f67cb0e8a45c537fac594ca3db6f684
holic5839 <holic5839@gmail.com>
2018-08-22 17:57:54 +0900

    css completed for header footer

--- src/app/yapen-footer.component.ts
+++ src/app/yapen-footer.component.ts
@@ -3,11 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-yapen-footer',
   template: `
-    <p>
-      yapen-footer works!!!
-    </p>
+<div class="yapenTailLayer">
+  <table cellpadding="0" cellspacing="0">
+      <tr>
+          <td style="text-align: right; width:360px;">
+              <img src="http://img.yapen.co.kr/pension/web/intro/yanoljaTravel.png" style="height:100px;" alt="야놀자트래블" />
+          </td>
+          <td style="padding-left:35px; text-align:left;">
+              <a href="/rules/terms?tab=1" target="_blank">이용약관</a> | <b><a href="/rules/terms?tab=2"
+                target="_blank">개인정보 처리방침</a></b> | <a href="/rules/terms?tab=3" target="_blank">개인정보 제3자 제공</a> |
+              <a href="/rules/terms?tab=4" target="_blank">위치기반 서비스 이용약관</a><br /><br />
+              (주)야놀자트래블 (대표이사 구본길)<br />
+              주소: 서울특별시 강남구 테헤란로 427<br />
+              고객센터: 1644-4816 (pension@yanolja.com)<br />
+              사업자등록번호: 261-81-03988<br />
+              통신판매업신고: 제2013-서울강남-03081호<br />
+              관광사업등록: 제2016-24호(서울특별시 강남구)<br />
+              <b>ⓒ</b> <label style="color:#FF6559;">YanoljaTravel</label> Corp.
+          </td>
+      </tr>
+  </table>
+</div>
+
   `,
   styles: [
+
   ]
 })
 export class YapenFooterComponent implements OnInit {
--- src/app/yapen-header.component.ts
+++ src/app/yapen-header.component.ts
@@ -4,14 +4,37 @@ import { Router } from '../../node_modules/@angular/router';
 @Component({
   selector: 'app-yapen-header',
   template: `
-    <a routerLink="/main">main   </a><a> | </a>
     <app-yapen-login (loginView)="loginView($event)" [(visible)]="visible"></app-yapen-login>
     <app-yapen-signup [(visible2)]="visible2"></app-yapen-signup>
-    <button *ngIf="!token" (click)="visible=!visible"> login </button><a>
-    <a *ngIf="token" routerLink="/main" (click)="logout()">로그아웃</a></a> |
-    <button (click)="visible2=!visible2">  signup</button><a> | </a>
+
+    <div class="yapenGuideHead">
+      <div class="headLayer">
+      <a (click)="visible2=!visible2">
+          <img src="https://img.yapen.co.kr/pension/images/web/join_text.png" alt="회원가입" />
+        </a>
+        <a *ngIf="!token" (click)="visible=!visible">
+          <img src="https://img.yapen.co.kr/pension/images/web/login_text.png" alt="로그인" />
+        </a>
+        <a *ngIf="token" routerLink="/main" (click)="logout()">
+          <img src="https://img.yapen.co.kr/pension/images/web/logout_text.png" alt="로그아웃" />
+        </a>
+      </div>
+    </div>
+    <div class="yapenHeader" id="yapenHeader">
+      <input type="text" name="" style="display:none;" />
+        <div class="topLayer">
+        <a routerLink="/main">
+          <img src="http://image2.yanolja.com/pension/new/yapen.png" alt="야놀자펜션" class="yapenLogo" /></a>
+        </div>
     `,
-  styles: [
+  styles: [`
+  .yapenHeader .topLayer {
+    color: #333;
+    position: relative;
+    text-align: center;
+    width: 1220px;
+    margin: 0px auto;
+}`
   ]
 })
 export class YapenHeaderComponent {
@@ -20,12 +43,12 @@ export class YapenHeaderComponent {
 
   token = localStorage.getItem('key');
 
-  constructor(private router: Router) {  }
+  constructor(private router: Router) { }
 
   logout() {
     if (this.token === null || this.token === '') {
-        alert('로그인 상태가 아닙니다.');
-        return;
+      alert('로그인 상태가 아닙니다.');
+      return;
     }
     alert('로그아웃 되었습니다.');
     this.router.navigate(['/main']);
