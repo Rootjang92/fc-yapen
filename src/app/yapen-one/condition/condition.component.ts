71b5bd7c7efce121a91f6d52a22a6a1caa4d3452
Rootjang92 <janggeunho7@gmail.com>
2018-08-23 18:01:54 +0900

    condition search url

--- src/app/yapen-one/condition/condition.component.ts
+++ src/app/yapen-one/condition/condition.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { StateviewService } from '../stateview.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, filter, scan, tap } from 'rxjs/operators';
 import { forEach } from '@angular/router/src/utils/collection';
+import { MomentModule } from 'angular2-moment';
 
 @Component({
   selector: 'app-condition',
@@ -14,20 +15,20 @@ import { forEach } from '@angular/router/src/utils/collection';
     <li style="width:260px" class="first-li" (click)="toggle('local')">
       <img src="../assets/glyphicons_free/glyphicons/png/glyphicons-39-plane.png" alt="지역" class="first-img">
       <div id="schLocationText">{{ res }}</div> <!-- 클릭하면 데이터가 바뀌도록  -->
-      <input type="hidden" name="schLoaction" id="schLoaction" value="인천/경기/가평">
-      <input type="hidden" name="schLoactionCode" id="schLoactionCode" value="1.001000">
+      <input type="hidden" name="schLoaction" id="schLoaction" [value]="res">
+      <input type="hidden" name="schLoactionCode" id="schLoactionCode" [value]="resid">
       <img src="../assets/glyphicons_free/glyphicons/png/glyphicons-602-chevron-down.png" alt="펼치기/닫기" class="arrowToggle last-img">
     </li>
-    <li style="width:250px" (click)="toggle()">
+    <li style="width:250px" (click)="toggle('calendar')">
       <img src="../assets/glyphicons_free/glyphicons/png/glyphicons-46-calendar.png" alt="날짜" class="first-img">
-      <div id="schStartDateText">2018/07/30 (1박 2일)</div>
-      <input type="hidden" name="schStartDate" id="schStartDate" value="2018/07/30">
-      <input type="hidden" name="schEndDate" id="schEndDate" value="2018/07/31">
+      <div id="schStartDateText">{{ searchDate() }} ({{ stayDate }})</div>
+      <input type="hidden" name="schStartDate" id="schStartDate" [value]="searchDate()">
+      <input type="hidden" name="schEndDate" id="schEndDate" [value]="getNextDate()">
       <img src="../assets/glyphicons_free/glyphicons/png/glyphicons-602-chevron-down.png" alt="펼치기/닫기" class="arrowToggle last-img">
     </li>
     <li style="width:260px" (click)="toggle('people')">
       <img src="../assets/glyphicons_free/glyphicons/png/glyphicons-500-family.png" alt="인원" class="first-img">
-      <div id="schPeopleText">{{ people }}</div>
+      <div id="schPeopleText">{{ people }}명</div>
       <input type="hidden" name="schPeople" id="schPeople" [value]="people">
       <img src="../assets/glyphicons_free/glyphicons/png/glyphicons-602-chevron-down.png" alt="펼치기/닫기" class="arrowToggle last-img">
       </li>
@@ -36,7 +37,8 @@ import { forEach } from '@angular/router/src/utils/collection';
       <div>테마 & 가격선택</div>
       <img src="../assets/glyphicons_free/glyphicons/png/glyphicons-602-chevron-down.png" alt="펼치기/닫기" class="arrowToggle last-img">
       </li>
-    <li style="width:182px; background-color:#ff6559; height: 42px;" class="last-li">
+    <li style="width:182px; background-color:#ff6559; height: 42px;" class="last-li"
+          (click)="searchRoom()">
       <img src="../assets/glyphicons_free/glyphicons/png/glyphicons-28-search.png" alt="찾기" class="first-img">
       <div>빈방찾기</div>
     </li>
@@ -45,14 +47,38 @@ import { forEach } from '@angular/router/src/utils/collection';
   `,
   styleUrls: [`./condition.css`]
 })
-export class ConditionComponent {
-  @Input() res: string;
+export class ConditionComponent implements OnInit {
+  @Input() res: any[];
+  @Input() resid: number;
   @Input() people = '전체';
+  @Input() periodid: number;
+  @Input() priceid: number;
+  @Input() themeid: string;
+  @Input() seletedDate;
+  @Input() stayDate;
+  @Input() basicPrice;
+  @Input() selectPrice;
+  @Input() stayPeriod;
+  @Input() stayDatevalue;
+  @Input() setThemeValue;
+  @Input() PriceValue;
+  @Input() Themes;
+  @Input() setTheme;
+  @Input() basicTheme;
+  location = [];
 
-  url = 'https://api.pmb.kr/location/location-name';
+
+
+
 
   constructor(public stateviewService: StateviewService,
               private http: HttpClient) { }
+
+  ngOnInit() {
+    // this.date = this.seletedDate.day;
+    // this.date.setDate( this.date.getDate() + this.stayDatevalue );
+    // console.log(this.date);
+  }
 
   toggle(state: string) {
     if (this.stateviewService.state === state) {
@@ -60,5 +86,32 @@ export class ConditionComponent {
     } else {
       this.stateviewService.state = state;
     }
+  }
+
+
+  searchDate() {
+    const dateS = `${this.seletedDate.year}-${this.seletedDate.month }-${ this.seletedDate.day }`;
+    // console.dir(this.seletedDate.day);
+    return dateS;
+  }
+
+  getNextDate() {
+    const nextDay = new Date(this.searchDate());
+    nextDay.setDate(nextDay.getDate() + this.stayDatevalue);
+    const next = `${nextDay.getFullYear()}/${nextDay.getMonth() + 1}/${nextDay.getDate()}`;
+    // console.log(next);
+    return next;
+  }
+
+  searchRoom() {
+    const conditionSearchUrl = 'https://api.pmb.kr/search/button_search/?'
+      + 'sub_location_no=' + this.resid + '&max_num_people=' + this.people +
+        '&price_range=' + this.basicPrice.value + '&checkin_date=' + this.searchDate() + '&'
+        + 'stay_day_num=' + this.stayDatevalue + '&theme=' + this.themeid;
+    console.log(conditionSearchUrl);
+    this.http.get<any[]>(conditionSearchUrl)
+      .subscribe( res => {
+        this.stateviewService.roomlist = res;
+      });
   }
 }
