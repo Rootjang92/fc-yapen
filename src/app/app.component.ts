71d614197b342cd89a8a968deb30762767cf1f78
holic5839 <holic5839@gmail.com>
2018-08-23 21:14:37 +0900

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-yapen-searchbar></app-yapen-searchbar>
-    <app-yapen-roomlist></app-yapen-roomlist>
   `,
   styleUrls: ['./app.component.css']
 })
